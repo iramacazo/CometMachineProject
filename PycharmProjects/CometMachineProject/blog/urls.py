707db1931931537e5835b7e5d425d1bfd6ed2d5c
Ira Macazo <ira_macazo@dlsu.edu.ph>
2017-06-12 19:33:09 +0800

    Added Comet Machine Project

--- PycharmProjects/CometMachineProject/blog/urls.py
+++ PycharmProjects/CometMachineProject/blog/urls.py
@@ -4,10 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
-    url(r'^login/$', auth_views.login, name='login'),
-
-    url(r'^register', views.register, name='register'),
-
-    url(r'^mainpage', views.mainpage, name='mainpage'),
 ]
